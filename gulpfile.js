b717a95ae7f8ccae166b9c40f324fe2fed718df2
Linat Timurshin <49582828+linat-timurshin@users.noreply.github.com>
2019-04-19 22:30:11 +0300

    Поясняющие комментарии в оглавлении)

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 "use strict";
 
-const { src, dest } = require("gulp");
-const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require('gulp-strip-css-comments');
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const cssnano = require("gulp-cssnano");
-const rigger = require("gulp-rigger");
-const uglify = require("gulp-uglify");
-const plumber = require("gulp-plumber");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const panini = require("panini");
-const browsersync = require("browser-sync").create();
+const { src, dest } = require("gulp"); // чтение и запись исходников
+const gulp = require("gulp"); // подключает GULP
+const autoprefixer = require("gulp-autoprefixer"); //автопрефикс
+const cssbeautify = require("gulp-cssbeautify"); //делаем css красиво
+const removeComments = require('gulp-strip-css-comments'); //удалет комментарии в *.min.css
+const rename = require("gulp-rename"); // добавляет *.min к файлам
+const sass = require("gulp-sass"); // компилятор SASS
+const cssnano = require("gulp-cssnano"); // сжимет CSS и удалет пробелы, и последние (;), делает весь CSS в одну строку
+const rigger = require("gulp-rigger"); // склеивает разные JS файлы в один
+const uglify = require("gulp-uglify"); // сжимает  JS файлы
+const plumber = require("gulp-plumber"); // при ошибке в JS не слетают Таски в Gulp
+const imagemin = require("gulp-imagemin"); // сжатие и оптимизация изображений
+const del = require("del"); // очищает папку Dist
+const panini = require("panini"); //  Работа с HTML, создает шаблоны и реализует фрагменты кода
+const browsersync = require("browser-sync").create(); // локальный сервер с live reload
+
 
 
 
